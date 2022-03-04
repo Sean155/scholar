270a60948e8da584d952f50a96ec218a58334d7a
yabaia <49870766+yabaia@users.noreply.github.com>
2022-03-04 15:59:55 +0800

    Refactoring

--- src/client.py
+++ src/client.py
@@ -3,48 +3,32 @@ from typing import Dict
 
 class client():
     
-    def __init__(self, proxy: bool = False) -> None:
-        self.client = Client
+    def __init__(self):
+
         self.headers: Dict = {
-                "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
-                #"Accept-Language": "zh-cn",
+                "User-Agent": 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1',
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                 }
-        self.proxies_: bool = proxy
-        if not self.proxies_:
-            self.proxies: str = None
-        else:
-            self.proxies: str = "http://127.0.0.1:7890"
-    
-    @classmethod     
-    def get(cls, url: str, proxy: bool) -> Response:
-        
-        return cls(proxy=proxy)._get(url=url)
+        self.proxies: str = "http://127.0.0.1:7890"
     
     @classmethod
-    def post(cls, url: str, json: Dict, proxy: bool) -> Response:
+    def get(cls, url: str, proxy: bool = True) -> Response:
         
-        return cls(proxy=proxy)._post(url=url, json=json)
-    
-    def _get(self, url: str, **kwargs) -> Response:
+        api = cls().call(proxy=proxy)
         
-        if self.proxies:
-            api = self.client(proxies=self.proxies, headers=self.headers)
-        else:
-            api = self.client(headers=self.headers)
         res = api.get(url=url, timeout=20)
         api.close()
         return res
-
-    def _post(self, url: str, json: Dict) -> Response:
+    
+    @classmethod
+    def post(cls, url: str, json: Dict, proxy: bool = True) -> Response:
         
-        if self.proxies:
-            api = self.client(proxies=self.proxies)
-        else:
-            api = self.client()
-        res = api.post(url=url, json=json)
+        api = cls().call(proxy=proxy)
+        res = api.post(url=url, json=json, timeout=20)
         api.close()
-        
         return res
     
-    
+    def call(self, proxy: bool) -> Client:
+        if proxy:
+            return Client(proxies=self.proxies, headers=self.headers)
+        return Client(headers=self.headers)
