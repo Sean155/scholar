da34055f7cf6b6ba67ac8a11bfbd18a8f99cdcdb
Sean155 <63349033+Sean155@users.noreply.github.com>
2022-03-06 09:33:34 +0800

    change button function, add loading 10 articles

--- src/main.py
+++ src/main.py
@@ -2,14 +2,11 @@ from PyQt5.QtWidgets import QMainWindow, QTableWidget, QTableWidgetItem, QApplic
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import sys
-import requests
-import sys
-import re
-from bs4 import BeautifulSoup
 import time
 from typing import List, Tuple
 from PyQt5 import QtCore, QtGui, QtWidgets
-from getsci import *
+from sqlalchemy import false
+from google_scholar import get_scholar, Scholar
 from google_translator import google_translator
 from mainwindow import *
 
@@ -19,64 +16,154 @@ class main_w2(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.download.clicked.connect(self.Download)
         self.search.clicked.connect(lambda :self.Search(self.keyword1.toPlainText()))
-        self.up.clicked.connect(lambda :self.Up(self.keyword1.toPlainText(),self.article_num.toPlainText()))
-        self.next.clicked.connect(self.Next)
-        self.go.clicked.connect(self.Go)
-#初始化设置
-        #self.Keywords=self.keyword1.toPlainText()
-        #self.articlenum=int(self.article_num.toPlainText())-1
+        self.up.clicked.connect(lambda :self.Up(self.keyword1.toPlainText(),self.num_gui.toPlainText()))
+        self.next.clicked.connect(lambda :self.Next(self.keyword1.toPlainText(),self.num_gui.toPlainText()))
+        self.go.clicked.connect(lambda :self.Go(self.keyword1.toPlainText(),self.num_gui.toPlainText()))
+        self.button_is_close=False
+        self.scholar : Scholar
+        self.artical_datebase = {}
+
+    def loading(self, artical_num) -> None:
+        '''
+        缓存摘要
+        '''
+        self.results_gui.setText(str(self.scholar.search_result_nums))
+        self.button_is_close = True
+        self.button_statu()
+        i = 0
+        for j in range(artical_num, artical_num+10):
+            self.artical_datebase[str(j)] = [self.scholar.artical[i], self.scholar.artical[i].abstract()]#访问变量修改
+            i +=1              
+            if i < 10:
+                time.sleep(4)
+            self.progressBar.setValue(i*10)
+            QApplication.processEvents()
+            
+        
+
+    def button_statu(self) -> None:
+        '''
+        改变按钮状态
+        '''
+        self.download.setDisabled(self.button_is_close)
+        self.search.setDisabled(self.button_is_close)
+        self.up.setDisabled(self.button_is_close)
+        self.next.setDisabled(self.button_is_close)
+        self.go.setDisabled(self.button_is_close)
+    
+
+    def redisplay(self, artical_num: int) -> None:
+        '''
+        根据论文号更新显示文献名、中英文摘要、第几篇文献、期刊、年份、数据库、作者
+        '''
+        a = self.artical_datebase[str(artical_num)][0]
+        artical_abstract = self.artical_datebase[str(artical_num)][1]
+        self.article_name.setText(a.name)
+        self.Abstract_1.setText(artical_abstract[0])
+        self.Abstract_2.setText(artical_abstract[1])
+        self.num_gui.setPlainText(str(artical_num))
+        self.journal_gui.setPlainText(a.journal)
+        self.year_gui.setText(a.year)
+        self.datebase_gui.setPlainText(a.database)
+        self.author_gui.setPlainText(a.author)
+
 
     def Download(self):
         '''
         定义下载按钮
         '''
+        self.msg()
+        self.download.setCheckable(True)
         print("download")
+        for i in range(10):
+            for j in range(11):
+                self.progressBar.setValue(i*10+j)
+                
+            time.sleep(1)
+            print("download")
         pass
 
 
-    def Search(self,key_words: str) -> None:
+    def Search(self, key_words: str) -> None:
         '''
         定义搜索按钮
         '''
-        print("search")
-        name, url = google_scholar(key_words,str(0))
-        abstract = get_abstract(name)
-        abstract_tran = google_translator().trans(abstract)
-        self.article_name.setText(name)
-        self.Abstract_1.setText(abstract)
-        self.Abstract_2.setText(abstract_tran)
+        self.msg()
+        self.search.setDisabled(True)
+        artical_num = 1
+        self.scholar = ''
+
+        self.abstracts = []
+        self.scholar = get_scholar(key_words,artical_num)
+        if self.scholar.statu:
+            self.loading(artical_num)
+            self.redisplay(artical_num)
+            self.button_is_close = False
+            self.button_statu()
+        else:
+            self.Abstract_1.setText(self.scholar.text)
 
 
     def Up(self,key_words: str,artical_num: str) -> None :
         '''
         定义上按钮
         '''
-        print("up")
+        self.up.setDisabled(True)
         artical_num= int(artical_num)-1
-        if artical_num>0:
-            name, url = google_scholar(key_words, str(artical_num-1))
-            abstract = get_abstract(name)
-            abstract_tran = google_translator().trans(abstract)
-            self.article_name.setText(name)
-            self.Abstract_1.setText(abstract)
-            self.Abstract_2.setText(abstract_tran)
-            self.article_num.setPlainText(str(artical_num))
+        if artical_num >0:
+            if not self.artical_datebase.get(str(artical_num)):
+                self.scholar = ''
+                self.scholar = get_scholar(key_words, artical_num)
+                if self.scholar.statu:
+                    self.loading(self) 
+                else:
+                    self.Abstract_1.setText(self.scholar.text)
+            self.redisplay(artical_num)
+            self.button_is_close = False
+            self.button_statu()
 
 
-    def Next(self):
+    def Next(self,key_words: str, artical_num: str) -> None:
         '''
         定义下按钮
         '''
-        print("next")
-        pass
+        self.next.setDisabled(True)
+        artical_num = int(artical_num)+1
+        if not self.artical_datebase.get(str(artical_num)):
+            self.msg()
+            self.scholar = ''
+            self.scholar = get_scholar(key_words, artical_num)
+            if self.scholar.statu:
+                self.loading(artical_num) 
+            else:
+                self.Abstract_1.setText(self.scholar.text)
+        self.redisplay(artical_num)
+        self.button_is_close = False
+        self.button_statu()
 
 
-    def Go(self):
+    def Go(self,key_words: str, artical_num: str):
         '''
         定义跳转按钮
         '''
-        print("go")
-        pass
+        self.go.setDisabled(True)
+        artical_num = int(artical_num)
+        if not self.artical_datebase.get(str(artical_num)):
+            self.scholar = ''
+            self.scholar = get_scholar(key_words, artical_num)
+            if self.scholar.statu:
+                self.loading(self) 
+            else:
+                self.Abstract_1.setText(self.scholar.text)
+        self.redisplay(artical_num)
+        self.button_is_close = False
+        self.button_statu()
+
+
+
+    def msg(self):
+        reply = QMessageBox.about(self,'提示','Waiting for loading abstract')
+        print(reply)
 
 
 #导入数据库,并存为data
