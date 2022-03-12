7055587a2af262f197b14eb3464b3c15d2c1b767
yabaia <49870766+yabaia@users.noreply.github.com>
2022-03-12 17:11:32 +0800

    Docstrings and exceptions

--- src/google_scholar.py
+++ src/google_scholar.py
@@ -1,26 +1,18 @@
 from bs4 import BeautifulSoup
-from utils import bs_find, str_replace, bs, T
+from .utils import bs_find, str_replace, bs, T, config, ProxyError, ConnectError, ConnectTimeout
 from typing import List, Tuple, Dict, Union
 import re
-from artical import get_artical, Artical
-
+from .artical import get_artical, Artical
+#import ptvsd
 
 class Scholar():
     '''
-    Scholar 类
-    
-    获取谷歌学术搜索的结果、整合文章信息
-    
-    类变量：
-    
-    search_result_nums：
-    结果条目数
-    
-    artical：
-    一页搜索结果的 Artical 类列表
-    
-    statu:
-    搜索状态，若为False，则搜索失败
+        :说明:
+          根据``key_words``获取谷歌学术搜索结果：
+          
+          ``author``, ``year``, ``database``, ``name``, ``url``, ``journal``
+          
+          若``Scholar.statu``为``False``, 则表示出现了``Scholar.text``中的异常
     '''
     
     search_result_nums: int
@@ -28,21 +20,34 @@ class Scholar():
     statu: bool = True
     text: str = ''
     
+    def __init__(self):
+        self.scholar_link = config.scholar_link + 'scholar?start={}&q={}&hl=zh-CN&as_sdt=0,5'
+    
     def search_results(self, key_words: str, artical_num: str) -> 'Scholar':
         '''
-        获取搜索结果
-        
-        返回一页（10条）结果
+            :说明:
+              获取搜索结果
+              返回一页（10条）结果
         '''
         artical_num = (int(artical_num)-1)
         key_words = str_replace([' '], key_words, '+')
-        scholar_link = f'https://scholar.google.com/scholar?start={artical_num}&q={key_words}&hl=zh-CN&as_sdt=0,5' 
-        #谷歌学术镜像https://xs2.dailyheadlines.cc/scholar?start={artical_num}&q={key_words}&hl=zh-CN&as_sdt=0,5
-        #https://scholar.google.com/scholar?start={artical_num}&q={key_words}&hl=zh-CN&as_sdt=0,5
-        #https://scholar.lanfanshu.cn/
-        #https://xs.dailyheadlines.cc/
-        scholar_result_soup = bs(scholar_link)
+        scholar_link = self.scholar_link.format(artical_num, key_words)
         
+        try:
+            scholar_result_soup = bs(scholar_link)
+        except ProxyError as e:
+            self.statu = False
+            self.text = f'Failed to get results from google scholar, please check your proxy-client.\n{e.args}'
+            return self
+        except ConnectError as e:
+            self.statu = False
+            self.text = f'Failed to get results from google scholar. You have set proxy true in config, but your proxy-client seems not work well.\n{e.args}'
+            return self
+        except ConnectTimeout as e:
+            self.statu = False
+            self.text = f'Failed to get results from google scholar, you can not access to google scholar. Please make sure you have set proxy true in config.\n{e.args}'
+            return self
+
         if not bs_find(scholar_result_soup, 'div', 'id', 'gs_res_ccl'):
             self.statu = False
             self.text = f'Failed to get results from google scholar, please check the Internet.:\n{scholar_result_soup.text}'
@@ -66,14 +71,16 @@ class Scholar():
     
     def get_artical_base_info(self, artical_info_all: T) -> Dict:
         '''
-        获取文章基本属性：
-        
-        作者、年份、期刊、数据库
+            :说明:
+              获取文章基本属性：
+              作者、年份、期刊、数据库
         '''
+        
         artical_time = bs_find(artical_info_all, 'div', 'class', 'gs_a').contents
-        artical_time = ''.join(i.string for i in artical_time if i)
+        artical_time = ''.join(i.string for i in artical_time if i if i.string)
         
         return self.format_base_info(artical_time)
+            
             
     def get_result_nums(self, scholar_result_soup: BeautifulSoup) -> int:
         '''
@@ -88,14 +95,16 @@ class Scholar():
         except AttributeError:
             return 1
         else:
-            search_result_nums = ''.join(i.string for i in search_result_nums if i)
+            search_result_nums = ''.join(i.string for i in search_result_nums if i if i.string)
             search_result_nums = re.compile(r'约\s(.*)\s条').search(search_result_nums)
         
         return int(str_replace([','], search_result_nums[1], ''))
     
+    
     def get_artical_name_url(self, artical_info_all: T) -> Tuple[str, Union[str, None], bool, str]:
         '''
-        获取文章标题、链接
+            :说明:
+              获取文章标题、链接
         '''        
         artical_name = bs_find(artical_info_all, 'h3', 'class', 'gs_rt')
         
@@ -112,11 +121,14 @@ class Scholar():
             statu = False
             text = f'Failed to get url: {name}\n'
             url = None
+            
         return name, url, statu, text
+    
     
     def format_base_info(self, string: str) -> Dict:
         '''
-        格式化文章基本属性
+            :说明:
+              格式化文章基本属性
         '''
         partern = re.compile(r'(.*)\s-\s(.*)\s-\s(.*)')
 
@@ -140,6 +152,7 @@ class Scholar():
             year = res_p[2]
         else:
             journal = 'book'
+            
         return {
             'author': author,
             'year': year,
@@ -150,11 +163,18 @@ class Scholar():
         }
 
 
-def get_scholar(key_words: str, search_page: str) -> 'Scholar':
+def get_scholar(key_words: str, artical_num: str) -> 'Scholar':
     '''
-    初始化 Scholar 类
+        :说明:
+          初始化``Scholar``类，
+          返回当前关键词下, 当前文章编号后的10条结果
+          
+        :参数:
+          * ``key_words: str``: 关键词
+          * ``artical_num: str``: 论文在搜索结果中的编号
     '''
-    return Scholar().search_results(key_words, search_page)
+    return Scholar().search_results(key_words, artical_num)
+
 
 if __name__ == '__main__':
     a = get_scholar('thermal', 2)
